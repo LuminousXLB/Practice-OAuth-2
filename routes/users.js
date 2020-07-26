856ea8e8c4c65512f5912ad62fff595df3d08f83
Taraxacum <john980118@163.com>
2020-07-26 17:18:58 +0800

    LocalStrategy

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,52 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function (username, password, done) {
+    if (username === password) {
+      return done(null, { username });
+    } else {
+      return done(null, false, { message: 'Incorrect username or password.' });
+    }
+  }
+));
+
+passport.serializeUser(function (user, done) {
+  done(null, user.username);
+});
+
+passport.deserializeUser(function (username, done) {
+  done(null, { username });
+});
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  if (req.query.user) {
+    res.render('hello', {
+      'title': 'Login Success',
+      'username': req.query.user
+    })
+  } else {
+    res.send('respond with a resource');
+  }
 });
+
+router.get('/login', function (req, res, next) {
+  res.render('login');
+});
+
+router.post('/login',
+  passport.authenticate('local', {
+    failureRedirect: '/login',
+    failureFlash: true
+  }),
+  function (req, res) {
+    // If this function gets called, authentication was successful.
+    // `req.user` contains the authenticated user.
+    res.redirect('/users?user=' + req.user.username);
+  }
+);
 
 module.exports = router;
